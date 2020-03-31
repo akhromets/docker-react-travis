303cf448a155212d1d4ab9c47fd2d3d3b8c325c3
Alexey Khromets <akhromets@webinerds.com>
2020-03-31 17:13:21 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Changed text from feature branch
         </a>
       </header>
     </div>
