7e1a26c1f938fd376d4cec269628b56251bcd4e7
Alexey Khromets <akhromets@webinerds.com>
2020-03-31 13:33:02 +0300

    revert app component to pass tests

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hi there
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
